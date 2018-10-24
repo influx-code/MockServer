d1e6f044b1072e3a6c6cced3cfe932f7c01d1978
devman <devman@influx.io>
2018-10-24 17:02:46 +0800

    栗子

--- router.conf.js
+++ router.conf.js
@@ -1,5 +1,13 @@
 module.exports = {
-    "/api/getlist": "./data/index/list.json",
-    "/api/123": "123",
-    "/": "./data/index/detail.json"
+    // 常规栗子
+    "/api/normal": "./data/md_name/normal.json",
+    /** ----------- 模板栗子 ---------------------- */
+    // 生成文本
+    "/api/string": "./data/md_name/string.json",
+    // 生成随机数
+    "/api/number": "./data/md_name/number.json",
+    // 生成对象
+    "/api/object": "./data/md_name/object.json",
+    // 生成数组
+    "/api/array": "./data/md_name/array.json"
 };
