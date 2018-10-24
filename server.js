ad98fbbacc486acf85a963c65c4b047867882e2b
devman <devman@influx.io>
2018-10-24 14:46:04 +0800

    up

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let server = {
         const self = this;
         let app = http.createServer((request, response) => {
             let url = request.url;
-            let router = require("./router");
+            let router = self.getRouter();
             if (!router[url]) {
                 // 404
                 self.handler404Code(response);
@@ -33,9 +33,10 @@ let server = {
      * @param {*} url 接口url
      */
     getAPIData(url) {
+        const self = this;
         return new Promise((resolve, reject) => {
             try {
-                let router = require("./router");
+                let router = self.getRouter();
                 let filepath = router[url];
                 let data = JSON.parse(
                     fs.readFileSync(path.resolve(__dirname, filepath), "utf-8")
@@ -49,14 +50,14 @@ let server = {
     /**
      * 404
      */
-    handler404Code: function(response) {
+    handler404Code(response) {
         response.writeHead(404, { "Content-Type": "text/plain" });
         response.end("Not Found");
     },
     /**
      * 500
      */
-    handler500Code: function(response) {
+    handler500Code(response) {
         response.writeHead(500, { "Content-Type": "text/plain" });
         response.end("Internal Server Error");
     },
@@ -64,9 +65,20 @@ let server = {
      * 200
      * @param {*} data
      */
-    handler200Code: function(response, data) {
+    handler200Code(response, data) {
         response.writeHead(200, { "Content-Type": "text/plain" });
         response.end(JSON.stringify(data));
+    },
+    /**
+     * 获取路由对象
+     */
+    getRouter() {
+        return JSON.parse(
+            fs.readFileSync(
+                path.resolve(__dirname, "./router.conf.json"),
+                "utf-8"
+            )
+        );
     }
 };
 
